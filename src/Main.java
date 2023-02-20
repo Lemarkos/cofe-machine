099b0c413d567d18a12e172f925648bec92eed6b
Lemarkos <sanjar.kasymkulov@gmail.com>
2023-02-20 20:23:09 +0600

    test commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         CommandLineService commandLineService = new CommandLineService();
         commandLineService.getOrder();
+        System.out.println("Hello");
     }
 }
